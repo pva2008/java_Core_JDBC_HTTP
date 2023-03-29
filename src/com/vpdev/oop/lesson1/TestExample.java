f7d47f092debff697a68a65b9a015acc8a3358f0
Vasili <pva2008@gmail.com>
2023-03-29 18:31:08 +0300

    1 + idea live templates

--- src/com/vpdev/oop/lesson1/TestExample.java
+++ src/com/vpdev/oop/lesson1/TestExample.java
@@ -3,13 +3,20 @@ package com.vpdev.oop.lesson1;
 public class TestExample {
     public static void main(String[] args) {
         int value = 10;
+
         int[] values = {value, 100, 199, 25};
-        printArray(values);
+        getPrintArray(values);
     }
 
-    private static void printArray(int[] values) {
+    private static void getPrintArray(int[] values) {
         for (int value : values) {
             System.out.println(value);
+
         }
     }
+
+
 }
+
+
+
