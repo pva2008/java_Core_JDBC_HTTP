d81d56baf0d86c71fe1b8cbbfdad75e4a9557e1f
Vasili <pva2008@gmail.com>
2023-03-29 19:09:59 +0300

    2 + Class+objects + Constructors

---
+++ src/com/vpdev/oop/lesson2/Computer.java
@@ -0,0 +1,30 @@
+package com.vpdev.oop.lesson2;
+
+public class Computer {
+    int ssd = 500;
+    int ram;
+
+    Computer() {
+        System.out.println("Я был создан");
+    }
+
+    Computer(int newSsd) {
+        ssd = newSsd;
+        ram = 999;
+    }
+
+    Computer(int ssd, int newRam) {
+        this.ssd = ssd;
+        ram = newRam;
+    }
+
+    void load() {
+        System.out.println("Я загрузился");
+    }
+
+    void printState() {
+        System.out.println("Ssd: " + ssd);
+        System.out.println("Ram: " + ram);
+        System.out.println();
+    }
+}
---
+++ src/com/vpdev/oop/lesson2/ComputerRunner.java
@@ -0,0 +1,14 @@
+package com.vpdev.oop.lesson2;
+
+public class ComputerRunner {
+    public static void main(String[] args) {
+        Computer computer = new Computer();
+        computer.printState();
+
+        Computer computer2 = new Computer(1050);
+        computer2.printState();
+
+        Computer computer3 = new Computer(10000, 10000);
+        computer3.printState();
+    }
+}
