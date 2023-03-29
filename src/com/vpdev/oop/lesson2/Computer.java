1638c3bd00f1fb89f4f6fae00d30616bd2af3715
Vasili <pva2008@gmail.com>
2023-03-29 19:25:37 +0300

    4 + method overloading

--- src/com/vpdev/oop/lesson2/Computer.java
+++ src/com/vpdev/oop/lesson2/Computer.java
@@ -1,27 +1,36 @@
 package com.vpdev.oop.lesson2;
 
 public class Computer {
-    int ssd = 500;
-    int ram;
 
-    Computer() {
-        System.out.println("Я был создан");
-    }
+    private int ssd = 500;
+    private long ram;
 
     Computer(int newSsd) {
         ssd = newSsd;
         ram = 999;
     }
 
-    Computer(int ssd, int newRam) {
+    protected Computer(int ssd, long newRam) {
         this.ssd = ssd;
         ram = newRam;
+    }
+
+    public Computer() {
+        System.out.println("Я был создан");
     }
 
     void load() {
         System.out.println("Я загрузился");
     }
 
+    void load(boolean open) {
+        System.out.println("Я загрузился");
+        if (open) {
+            System.out.println("Я открыл крышку");
+        }
+    }
+
+    //  [модификаторы] returnValue name([params])
     void printState() {
         System.out.println("Ssd: " + ssd);
         System.out.println("Ram: " + ram);
