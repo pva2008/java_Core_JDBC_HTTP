0f8da76a0e91045982c8e61727d87330afd17482
Vasili <pva2008@gmail.com>
2023-04-06 18:01:49 +0300

    43 + Iterator->List

--- src/com/vpdev/oop/lesson19/iterator/IteratorExample.java
+++ src/com/vpdev/oop/lesson19/iterator/IteratorExample.java
@@ -7,7 +7,6 @@ import java.util.List;
 public class IteratorExample {
 
     public static void main(String[] args) {
-
 //                                              |
         List<Integer> sourceList = List.of(1, 2, 3, 4, 5, 6);
         List<Integer> list = new ArrayList<>(sourceList);
@@ -15,25 +14,12 @@ public class IteratorExample {
 //        for (int i = 0; i < list.size(); i++) {
 //            Integer integer = list.get(i);
 //        }
-//        for (Integer integer : list) {
-//            System.out.println(integer);
-//        }
-        Iterator<Integer> iterator = sourceList.iterator();
-        while (iterator.hasNext()) {
-            Integer next = iterator.next();
+        for (Integer integer : list) {
+            System.out.println(integer);
         }
-
-//        for (Iterator<Integer> iterator = list.iterator(); iterator.hasNext(); ) {
-//            Integer next = iterator.next();
-//            if (next == 3 || next == 4) {
-//                iterator.remove();
-//            }
-//        }
-        System.out.println(list);
-//        Iterator<Integer> iterator = list.iterator();
-//        while (iterator.hasNext()) {
-//            Integer next = iterator.next();
-//            System.out.println(next);
-//        }
+        for (Integer integer : list) {
+            System.out.println(integer);
+        }
+        list.forEach(System.out::println);
     }
 }
